5a45d1007a60ec78c611c0d7e49588e9a19c0f29
jamstart36 <jamsiandm@gmail.com>
2026-02-15 10:28:58 +0800

    Updates

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,8 +1,8 @@
 export const FooterSection=()=>{
     return(
-        <div className="p-2 h-[8vh]">
+        <div className="p-3 h-[8vh]">
             <div className="fontfamily bgfooter flex justify-center items-center rounded-xl h-full shadow-md">
-                <p className="text-white text-[15px]">Copyright 2026 Jams Ian's Portfolio | All Rights Reserved.</p>
+                <p className="text-white text-[10px] sm:text-[12px] md:text-[15px]">Copyright 2026 Jams Ian's Portfolio | All Rights Reserved.</p>
             </div>
         </div>
     );
